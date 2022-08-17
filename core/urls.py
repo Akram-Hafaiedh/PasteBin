fb2af9e58d7a4bb4eaf62c1786c67e0cbe21a59b
Akram <hfaiedhakram@gmail.com>
2022-08-17 11:36:32 +0100

    Reponse object instead of JsonReponse

--- core/urls.py
+++ core/urls.py
@@ -20,13 +20,13 @@ from django.urls import include, path
 from rest_framework import routers
 #Local
 from core.quickstart import views
-
+    
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    path('',include(router.urls)),
+    path('',include('snippets.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
